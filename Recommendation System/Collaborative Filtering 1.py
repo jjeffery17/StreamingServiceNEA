8fb97dfce454a2618a916340655688e5be46dea5
James <jjeffery17@heathfieldcc.co.uk>
2023-01-25 18:26:21 +0000

    put entire function into a class and tested efficiency. calculating cosine similarity is extremely time-consuming and will most likely by nearly unusable on very large datasets. at the moment, it takes roughly 6.5 seconds to calculate recommendations for a 1000x1000 matrix

--- Recommendation System/Collaborative Filtering 1.py
+++ Recommendation System/Collaborative Filtering 1.py
@@ -1,43 +1,64 @@
 import numpy as np
 import time
 
-#creating example array
-'array = np.zeros((5,10)) #empty array'
-array = np.random.uniform(low=-1.0, high=1.0, size=(50, 100)) #random array
+
+class CollaborativeFiltering():
+    def __init__(self, size=(100, 100)):
+        #creating example array
+        #array = np.zeros((5,10)) #empty array
+        start = time.time()
+        self.array = np.random.uniform(low=-1.0, high=1.0, size=size) #random array
+        end = time.time()
+        print("Time to create:", end - start, "s for", self.array.size, "items\n")
 
 
-def cosine_similarity(a, b, output_degrees=False):
-    dot = np.dot(a, b)
-    mag_a = np.sqrt(a.dot(a)) #magnitude of a
-    mag_b = np.sqrt(b.dot(b)) #magnitude of b
-    reciprocal = 1.0/(mag_a*mag_b) #using the reciprocal and multiplying instead of dividing to increase efficiency
-    if output_degrees:
-        return np.degrees(np.arccos(dot*reciprocal))
-    else:
-        return dot*reciprocal
+    def cosine_similarity(self, a, b, output_degrees=False): #TODO: make much more efficient
+        dot = np.dot(a, b)
+        mag_a = np.sqrt(a.dot(a)) #magnitude of a
+        mag_b = np.sqrt(b.dot(b)) #magnitude of b
+        reciprocal = 1.0/(mag_a*mag_b) #using the reciprocal and multiplying instead of dividing to increase efficiency
+        if output_degrees:
+            return np.degrees(np.arccos(dot*reciprocal))
+        else:
+            return dot*reciprocal
 
+    def find_recommendations(self):
+        startTotal = time.time() #measure efficiency
+        recommendations = []
+        for user_index in range(len(self.array)): #iterating through users
+            self.__startPartner = time.time()
+            similarity = []
+            for partner in self.array:
+                #comparing user with partner
+                if not np.array_equal(partner, self.array[user_index]): #don't compare with itself
+                    similarity.append(self.cosine_similarity(self.array[user_index], partner, output_degrees=False))
+                else:
+                    similarity.append(-1.0)
+            most_similar_partner_index = similarity.index(max(similarity)) #find best partner
+            self.__endPartner = time.time()
 
-def find_recommendations(array):
-    start = time.time() #measure efficiency
-    recommendations = []
-    for user_index in range(len(array)): #iterating through users
-        similarity = []
-        for partner in array:
-            #comparing user with partner
-            if not np.array_equal(partner, array[user_index]): #don't compare with itself
-                similarity.append(cosine_similarity(array[user_index], partner, output_degrees=False))
-            else:
-                similarity.append(-1.0)
-        most_similar_partner_index = similarity.index(max(similarity)) #find best partner
+            #finding items to recommend
+            self.__startMovement = time.time()
+            for item_index in range(len(self.array[user_index])):
+                if self.array[user_index][item_index] > -0.1 and self.array[user_index][item_index] < 0.1: #find undiscorvered items
+                    if self.array[most_similar_partner_index][item_index] > 0.8: #find if partner likes undiscovered item
+                        recommendations.append([user_index, item_index])
+            self.__endMovement = time.time()
+        endTotal = time.time()
+        print("Time to find partner:", (self.__endPartner - self.__startPartner)*self.array.shape[0], "s for", self.array.size, "items\n")
+        print("Time to move:", self.__endMovement - self.__startMovement, "s for", self.array.size, "items\n")
+        print("Time to recommend:", endTotal-startTotal, "s for", self.array.size, "items\n")
+        return recommendations
 
-        #finding items to recommend
-        for item_index in range(len(array[user_index])):
-            if array[user_index][item_index] > -0.1 and array[user_index][item_index] < 0.1: #find undiscorvered items
-                if array[most_similar_partner_index][item_index] > 0.8: #find if partner likes undiscovered item
-                    recommendations.append([user_index, item_index])
-    end = time.time()
-    #print("Time to complete:", end-start, "s for", array.size, "items\n")
-    return recommendations
+def testEfficiency():
+    for x in range(4):
+        for y in range(4):
+            size = (10**x, 10**y)
+            model = CollaborativeFiltering(size=size)
+            model.find_recommendations()
+            print(size)
+
+testEfficiency()
 
 '''
 #outputing recommendations
