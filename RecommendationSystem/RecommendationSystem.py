c4e7dd6c9e786dba1654e8a2934e0df91cd3b7ff
James <jjeffery17@heathfieldcc.co.uk>
2023-02-17 17:59:50 +0000

    added system to update values for collaborative filtering. not yet implimented into a database

--- RecommendationSystem/RecommendationSystem.py
+++ RecommendationSystem/RecommendationSystem.py
@@ -1,5 +1,5 @@
-from RecommendationSystem import CollaborativeFiltering as CF
-
+from RecommendationSystem import CollaborativeFiltering as CF #import as called from main.py
+import math
 def getRecommendations(userID, amountPerType):
     recommendations = []
 
@@ -15,4 +15,20 @@ def getRecommendations(userID, amountPerType):
 
     return recommendations
 
-#print(getRecommendations(1, [10, 10, 5]))
+def updateItem(userID, songID, rating, listened, artist, album):
+    '''
+    :param userID: userID for collaborative filtering location
+    :param songID: songID for collaborative filtering location
+    :param rating: star rating (1-5) given by user (default 3)
+    :param listened: % of song listed (includes multiple listens e.g. 300% = 3 complete listens)
+    :param artist: amount of songs listened to by same artist
+    :param album: % of album listened to (includes multiple listens)
+    :return:
+    '''
+
+    rating = (rating-3)*0.5
+    listened = 2*(math.tanh(listened))-1
+    artist = math.log(artist+1) #natural log
+    album = math.tanh(album)
+    value = math.tanh(rating+listened+artist+album)
+    print(". updating song:", songID, "for user:", userID, "to value:", value)
--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-from UserInterface import UserInterface as UI
-from RecommendationSystem import RecommendationSystem as RS
+from UserInterface import UserInterface as ui
+from RecommendationSystem import RecommendationSystem as rs
 
-recommendations = RS.getRecommendations(1, [10, 10, 5])
+recommendations = rs.getRecommendations(1, [10, 10, 5])
 print("- final recommendations:", recommendations)
-UI.runUI(recommendations)
+
+#testing updating item
+rs.updateItem(0, 0, 5, 6.88, 34, 3.5) #likes song
+rs.updateItem(0, 0, 1, 0.1, 0, 0.05) #dislkies song
+rs.updateItem(0, 0, 3, 1, 0, 0.05) #no interaction
+
+ui.runUI(recommendations)
